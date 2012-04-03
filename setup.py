2fe51731ab102f4a70e8dad344bccee202154dde
Rory McCann <rory@technomancy.org>
2012-04-03 09:46:26 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name="podiff",
+      version="0.1",
+      author="Rory McCann",
+      author_email="rory@technomancy.org",
+      py_modules=['podiff'],
+)
